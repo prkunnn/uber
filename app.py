38f182bc4363712f86abe4fe043cd7d47b7ac6fa
DanielGit0w0 <sebastian.will0w0@gmail.com>
2025-01-01 19:25:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('testOne'))
+    return redirect(url_for('frontPage'))
 
 # Route: View Orders
 @app.route('/orders', methods=['GET'])
@@ -173,11 +173,11 @@ def update_order_status():
 @app.route('/', methods=['GET'])
 def home():
     if 'customer_id' not in session:
-        return redirect(url_for('testOne'))  # 如果未登入，重定向到主登入頁面
+        return redirect(url_for('frontPage'))  # 如果未登入，重定向到主登入頁面
     else:
         return redirect(url_for('main_index'))
     if 'merchant_id' not in session:
-        return redirect(url_for('testOne'))  # 如果未登入，重定向到登入頁面
+        return redirect(url_for('frontPage'))  # 如果未登入，重定向到登入頁面
     return render_template('home.html')  # 渲染主頁模板
 
 @app.route('/menu', methods=['GET', 'POST'])
@@ -226,6 +226,14 @@ def manage_menu():
     conn.close()
 
     return render_template('view_menu.html', menu_items=menu_items)
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Route: Front Page登入前主頁
+@app.route("/frontPage")
+def frontPage():
+	#data = dat
+	return render_template('index.html')
 
 # Route: 客人 Register
 @app.route('/customer_regi', methods=['GET', 'POST'])
@@ -285,7 +293,7 @@ def main_index():
         return redirect(url_for('cuslogin'))
     return render_template('main_index.html')
 
-# 餐廳列表
+# Route:餐廳列表
 @app.route("/restaurants")
 def restaurants():
     conn = get_db_connection()
@@ -337,11 +345,13 @@ def chcekSC():
 # Route: 確認訂單
 @app.route("/confirm_order", methods=['POST'])
 def confirm_order():
+    # loaded = False
     customer_id = session['customer_id']
     address = session['customer_address']
     if request.method == 'POST':
         merchant_id = request.form['m_id']
         total_price = request.form['total_price']
+        # loaded = request.form['loaded']
         items = []
         
         for key, value in request.form.items():
@@ -351,17 +361,49 @@ def confirm_order():
                 dish_id = request.form.get(f'dish_id_{item_id}')
                 price = request.form.get(f'sum_price_{item_id}')
                 items.append({'dish_id': dish_id, 'amount': amount, 'price': price})
+
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True)
+        # 检查是否已经存在相同的订单
+        cursor.execute("SELECT id FROM `order` WHERE customer_id = %s AND merchant_id = %s AND delivery_address = %s AND total_price = %s ORDER BY created_at DESC LIMIT 1;", (customer_id, merchant_id, address, total_price))
+        last_order = cursor.fetchone()
+        # cursor.execute("SELECT id FROM `order` WHERE customer_id = %s ORDER BY created_at DESC LIMIT 1;", (customer_id,))
+        # last_order = cursor.fetchone() #135
+        cursor.execute("SELECT TIMESTAMPDIFF(SECOND,(SELECT created_at FROM `order` ORDER BY id DESC LIMIT 1 OFFSET 1), (SELECT created_at FROM `order` ORDER BY id DESC LIMIT 1 )) as timediff;")
+        time_diff = cursor.fetchone()
+        time_diff_str = str(time_diff)
+        # return jsonify(last_order)
+        if last_order:
+            # 检查订单项是否相同
+            cursor.execute("SELECT menu_item_id, quantity, price FROM `orderitem` WHERE order_id = %s", (last_order['id'],))
+            existing_items = cursor.fetchall()
+            existing_items_set = {(str(item['menu_item_id']), str(item['quantity']), str(item['price'])) for item in existing_items}
+            new_items_set = {(item['dish_id'], item['amount'], item['price']) for item in items}
+            # return jsonify(list(existing_items_set), list(new_items_set))
+            # return jsonify(existing_items, items, existing_items_set)
+
+            if existing_items_set == new_items_set:
+                # 如果订单项相同，则不插入新的订单
+                cursor.execute("SELECT DISTINCT id, status, total_price, delivery_person_id FROM `order` WHERE customer_id = %s ORDER BY created_at DESC LIMIT 1;", (customer_id,))
+                order = cursor.fetchall()
+                cursor.execute("SELECT name FROM `deliveryperson` WHERE id = %s", (order[0]['delivery_person_id'],))
+                deliver = cursor.fetchall()
+                cursor.execute("SELECT orderitem.menu_item_id, orderitem.quantity, orderitem.price, menuitem.name FROM `orderitem`,`menuitem` WHERE order_id = %s AND menuitem.id=orderitem.menu_item_id", (order[0]['id'],))
+                fitem = cursor.fetchall()
+                conn.close()
+                return render_template('cusOrder.html', order=order, fitem=fitem, deliver=deliver)
+
+        # 插入新的訂單
         cursor.execute("INSERT INTO `order` (customer_id, merchant_id, delivery_address, total_price) VALUES (%s,%s,%s,%s);", (customer_id, merchant_id, address, total_price,))
-        
         for item in items:
             cursor.execute("INSERT INTO `orderitem` (menu_item_id, quantity, price) VALUES (%s,%s,%s);", (item['dish_id'], item['amount'], item['price'],))
             cursor.fetchone()
             #order.append(cursor.fetchone())
         cursor.execute("UPDATE orderitem SET order_id = (SELECT id FROM `order` ORDER BY created_at DESC LIMIT 1) Where order_id IS NULL;")
-        cursor.execute("SELECT DISTINCT id, status, total_price FROM `order` WHERE customer_id = %s ORDER BY created_at DESC LIMIT 1;", (customer_id,))
+        cursor.execute("SELECT DISTINCT id, status, total_price, delivery_person_id FROM `order` WHERE customer_id = %s ORDER BY created_at DESC LIMIT 1;", (customer_id,))
         order = cursor.fetchall()
+        cursor.execute("SELECT name FROM `deliveryperson` WHERE id = %s", (order[0]['delivery_person_id'],))
+        deliver = cursor.fetchall()
         cursor.execute("SELECT orderitem.menu_item_id, orderitem.quantity, orderitem.price, menuitem.name FROM `orderitem`,`menuitem` WHERE order_id = %s AND menuitem.id=orderitem.menu_item_id", (order[0]['id'],))
         fitem = cursor.fetchall()
         # return jsonify(order)
@@ -370,29 +412,32 @@ def confirm_order():
         cursor.close()
         conn.close()
 
-    return render_template('cusOrder.html', order=order, fitem=fitem)
+    return render_template('cusOrder.html', order=order, fitem=fitem, deliver=deliver)
 
 # Route: 收貨
 @app.route("/confirm_delivered", methods=['POST'])
 def confirm_delivered():
     customer_id = session['customer_id']
     # 在这里处理确认收到订单的逻辑，例如更新订单状态
-    # 示例代码：
+    if request.method == 'POST':
+        order_id = request.form['o_id']
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("UPDATE `order` SET status = 'Delivered' WHERE customer_id = %s ORDER BY created_at DESC LIMIT 1;", (customer_id,))
+    cursor.execute("UPDATE `order` SET status = 'Received' WHERE customer_id = %s AND id = %s AND status = %s;", (customer_id, order_id, 'Delivered',))
+    cursor.execute("UPDATE `deliveryperson` SET current_status = 'Idle' WHERE id = (SELECT delivery_person_id FROM `order` WHERE customer_id = %s AND id = %s AND status = 'Received');", (customer_id,order_id,))
     conn.commit()
     cursor.close()
     conn.close()
     # return redirect(url_for('home'))
     return redirect("/")
 
+# Route: 訂單歷史
 @app.route("/order_history")
 def order_history():
     customer_id = session['customer_id']
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT id, status, total_price FROM `order` WHERE customer_id = %s ORDER BY created_at DESC;", (customer_id,))
+    cursor.execute("SELECT * FROM `order` WHERE customer_id = %s ORDER BY created_at DESC LIMIT 30;", (customer_id,))
     orders = cursor.fetchall()
     order_details = []
     for order in orders:
@@ -408,22 +453,31 @@ def order_history():
     conn.close()
     return render_template('order_history.html', orders=orders, order_details=order_details)
 
-@app.route("/about")
-def about():
-	#data = dat
-	return render_template('about.html')
+@app.route('/check_single_order', methods=['POST'])
+def check_order():
+    customer_id = session['customer_id']
+    address = session['customer_address']
+    if request.method == 'POST':
+        order_id = request.form['o_id']
+        merchant_id = request.form['m_id']
+        total_price = request.form['total_price']
+        deliver_id = request.form['d_id']
+    # return jsonify(items)
+    
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM `order` WHERE id = %s", (order_id,))
+    order = cursor.fetchall()
+    cursor.execute("SELECT orderitem.menu_item_id, orderitem.quantity, orderitem.price, menuitem.name FROM `orderitem`,`menuitem` WHERE order_id = %s AND menuitem.id=orderitem.menu_item_id", (order_id,))
+    fitem = cursor.fetchall()
+    cursor.execute("SELECT name FROM `deliveryperson` WHERE id = %s", (deliver_id,))
+    deliver = cursor.fetchone() or []
+    cursor.close()
+    conn.close()
+    
+    return render_template('cusOrder.html', order=order, fitem=fitem, deliver=deliver)
 
-@app.route("/news")
-def news():
-	#data = dat
-	return render_template('news.html')
-
-@app.route("/test")
-def testOne():
-	#data = dat
-	return render_template('index.html')
-
-# Route: View Menu Items
+# Route: View Menu Items 查看各餐廳菜單
 @app.route('/check_menu', methods=['GET'])
 def check_menu():
     if 'customer_id' not in session:
@@ -447,24 +501,35 @@ def check_menu():
     
     return render_template('check_menu.html', merchant = merchant_info, menu_items =menu_items, feedbacks = feedbacks)
 
-@app.route('/feedback', methods=['POST'])
+# Route: Feedback給評
+@app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     if 'customer_id' not in session:
         return redirect(url_for('cuslogin'))
 
+    rating = request.form.get('star_count')
+    print(f'Star count: {rating}')
+    # 處理新增點評項目
+    c_id = session['customer_id']
+    if request.method == 'POST':
+        if 'm_id' in request.form:
+            target_id = request.form['m_id']
+            feedback_text = request.form['feedback']
+            rating = request.form['star_count']
+        elif 'd_id' in request.form:
+            target_id = int(request.form['d_id'])+10000
+            feedback_text = request.form['feedback']
+            rating = request.form['star_count']
+            order_id = request.form['o_id']
+            target_id = str(target_id)+'_'+str(order_id)
+    else:
+        target_id = "No target id provided"
+        feedback_text = 'No feedback or rating provided'
+    # return jsonify(feedback_text, target_id, rating)
+    
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
-
-    # 處理新增點評項目
-    if request.method == 'POST':
-        c_id = session['customer_id']
-        # availability_status = request.form['availability_status']
-        m_id = request.form['m_id']
-        feedback_text = request.form['feedback']
-    else: 
-        feedback_text = 'No feedback provided'
-    #return jsonify(m_id)
-    cursor.execute("INSERT INTO feedback (customer_id, target_id, feedback_text, rating, created_at) VALUES (%s, %s, %s, %s, NOW());", (c_id, m_id, feedback_text, 5,))
+    cursor.execute("INSERT INTO feedback (customer_id, target_id, feedback_text, rating, created_at) VALUES (%s, %s, %s, %s, NOW());", (c_id, target_id, feedback_text, rating,))
     conn.commit()
     # cursor.execute("""
     #         INSERT INTO feedback (feedback_text)
@@ -479,7 +544,142 @@ def feedback():
 
     return redirect(url_for('restaurants'))
 
+# Route: About關於我們頁面
+@app.route("/about")
+def about():
+	#data = dat
+	return render_template('about.html')
 
+# Route: News最新消息, Blog頁面
+@app.route("/news")
+def news():
+	#data = dat
+	return render_template('news.html')
+#-------------------------------------------------------------------------------------------------------------
+
+
+# Route: Deliver Registration
+@app.route('/deliver_regi', methods=['GET', 'POST'])
+def deliver_regi():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        # Hash the password before saving to the database
+        hashed_password = generate_password_hash(password)
+
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        try:
+            cursor.execute("INSERT INTO deliveryperson (name, contact_info, password) VALUES (%s, %s, %s)",
+                           (name, email, password))
+            conn.commit()
+            return redirect(url_for('deliver_login'))
+        except mysql.connector.Error as err:
+            return jsonify({'error': str(err)})
+        finally:
+            cursor.close()
+            conn.close()
+    return render_template('deliverRegister.html')
+
+# Route: Deliver Login
+@app.route('/deliver_login', methods=['GET', 'POST'])
+def deliver_login():
+    if request.method == 'POST':
+        phone = request.form['email']
+        password = request.form['password']
+
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM deliveryperson WHERE contact_info = %s", (phone,))
+        deliver = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        # return jsonify(deliver['password'], password)
+
+        # if deliver and check_password_hash(deliver['password'], password):
+        if deliver['password'] == int(password):
+            session['deliver_id'] = deliver['id']
+            session['deliver_name'] = deliver['name']
+            return redirect(url_for('deliverHome'))
+        else:
+            return jsonify({'error': 'Invalid email or password'})
+    return render_template('deliveryLogin.html', merchant_login=True)
+
+# Route: Deliver Home
+@app.route('/driverHome', methods=['GET'])
+def deliverHome():
+    if 'deliver_id' not in session:
+        return redirect(url_for('deliver_login'))
+    deliver_name = session['deliver_name']
+    deliver_id = session['deliver_id']
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT id, status FROM `order` WHERE delivery_person_id = %s AND status = %s or status =  %s", (deliver_id,'Accepted', 'PickedUp'))
+    order_id = cursor.fetchall()
+
+    # cursor.fetchall()
+    cursor.close()
+    conn.close()
+    # return jsonify(order_id)
+    return render_template('driver_home.html', deliver_name=deliver_name, order_id=order_id)
+
+
+# Route: View Assigned Orders
+@app.route('/assigned_orders', methods=['GET'])
+def assigned_orders():
+    if 'deliver_id' not in session:
+        return redirect(url_for('deliver_login'))
+    # return render_template('assigned_orders.html')
+
+    deliver_id = session['deliver_id']
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    # 查詢該商家所有訂單
+    cursor.execute("""
+        SELECT o.id AS order_id, o.status, o.delivery_address, o.total_price, o.created_at,
+               c.name AS customer_name, m.name AS merchant_name, m.contact_info 
+        FROM `merchant` m, `order` o
+        JOIN customer c ON o.customer_id = c.id
+        WHERE o.status = "Pending"
+        AND o.merchant_id = m.id
+        ORDER BY o.created_at ASC
+    """)
+    orders = cursor.fetchall()
+    cursor.execute("SELECT current_status FROM `deliveryperson` WHERE id = %s", (deliver_id,))
+    status = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    return render_template('assigned_orders.html', orders=orders, status=status)
+
+# Route: Update Order Status
+@app.route('/upd_order_status', methods=['POST'])
+def upd_order_status():
+    if 'deliver_id' not in session:
+        return redirect(url_for('deliver_login'))
+
+    order_id = request.form['order_id']
+    new_status = request.form['status']
+    deliver_id = session['deliver_id']
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        UPDATE `order` SET status = %s, delivery_person_id = %s WHERE id = %s 
+    """, (new_status,  deliver_id, order_id),)
+    conn.commit()
+    cursor.execute("""
+        UPDATE `deliveryperson` SET current_status = %s WHERE id = %s 
+    """, (new_status, deliver_id),)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return redirect(url_for('deliverHome'))
 
 if __name__ == '__main__':
     app.run(debug=True)
